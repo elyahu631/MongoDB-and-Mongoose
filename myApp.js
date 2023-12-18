4d31aae86acbb9d16e5c02e97bcc0102dbbc73ea
elyahu631 <eanavi363@gmail.com>
2023-12-18 09:01:38 +0200

    Create Many Records with model.create()

--- myApp.js
+++ myApp.js
@@ -33,7 +33,10 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople,(err,people) => {
+    if (err) return done(err);
+    done(null , people);
+  })
 };
 
 const findPeopleByName = (personName, done) => {
