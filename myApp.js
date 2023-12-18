67d7940f1e43b529ee2765bf53fff49b00e09783
elyahu631 <eanavi363@gmail.com>
2023-12-18 08:25:05 +0200

    update db

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected to the database');
+});
 
 let Person;
 
